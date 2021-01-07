b57b6630220d4054557e3adbf0583fe3db6acca8
Aman5797 <mdaman5797@gmail.com>
2021-01-07 16:16:20 +0530

    v2 Initial commit

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const lodash = require("lodash");
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://admin-aman:Albertcr7007@cluster0.ir7gf.mongodb.net/blogDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const posts = [];
+const postSchema = mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const post1 = new Post({
+  title: "Python",
+  content:
+    "Future of python developers is so bright that every other delopment stream will be in shadow.",
+});
+//post1.save();
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts,
+  Post.find({}, function (err, posts) {
+    if (!err) {
+      console.log(posts);
+      res.render("home", {
+        posts: posts,
+      });
+    }
   });
 });
 
 app.get("/about", function (req, res) {
-  res.render("about", { about: aboutContent });
+  res.render("about", {});
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact", { contact: contactContent });
+  res.render("contact", {});
 });
 
 app.get("/compose", function (req, res) {
@@ -39,21 +57,30 @@ app.get("/compose", function (req, res) {
 });
 
 app.get("/posts/:param", function (req, res) {
-  posts.forEach(function (post) {
-    if (lodash.kebabCase(req.params.param) === lodash.kebabCase(post.title)) {
-      console.log("Matched");
-      res.render("post", { title: post.title, content: post.content });
+  Post.find({}, function (err, posts) {
+    if (!err) {
+      posts.forEach(function (post) {
+        if (req.params.param == post._id) {
+          console.log(req.params.param + "||");
+          console.log(post._id + "||");
+          console.log("Matched");
+          res.render("post", { title: post.title, content: post.content });
+        }
+      });
+    } else {
+      console.log("Error:65");
     }
   });
+
   console.log(req.params.param);
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-  };
-  posts.push(post);
+  });
+  post.save();
   res.redirect("/");
 });
 
